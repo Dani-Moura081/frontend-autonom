a35c6a471a3e0966e8a6f122673deecebc340534
arthur044 <arthuralima.com.br@gmail.com>
2024-10-19 15:53:27 -0300

    3/4

--- src/components/cardEspecificacao/index.jsx
+++ src/components/cardEspecificacao/index.jsx
@@ -3,7 +3,7 @@ import "./index.scss";
 export default function CardEspecifications() {
   return (
     <div className="comp-content-mid">
-      <h2>O que nós oferecemos</h2>
+      <h1>O que nós oferecemos</h1>
         <p>
           Oferecemos uma gama abrangente de serviços econômicos para
           propriedades residenciais e comerciais
@@ -16,7 +16,7 @@ export default function CardEspecifications() {
               src="/src/assets/eletricImg1.svg"
               alt="imagem placa eletrica"
             />
-            <h3>Preventiva</h3>
+            <h2>Preventiva</h2>
             <p>
               A <b>manutenção elétrica preventiva</b> é uma inspeção feita de
               tempos em tempos nas instalações elétricas para antecipar
@@ -29,7 +29,7 @@ export default function CardEspecifications() {
               src="/src/assets/eletricImgLampada.svg"
               alt="imagem lampada cinza"
             />
-            <h3>Instalação elétrica</h3>
+            <h2>Instalação elétrica</h2>
             <p>
               <b>Infraestrutura,</b>
               <b> Cabeamento</b>,<b> Quadro elétrico principal</b>;{" "}
@@ -42,7 +42,7 @@ export default function CardEspecifications() {
               src="/src/assets/eletricImgTomada.svg"
               alt="imagem tomada cinza"
             />
-            <h3>Reparos & Manutenção</h3>
+            <h2>Reparos & Manutenção</h2>
             <p>
               <b>Reparo e manutenção elétrica </b>envolve o diagnóstico, correção de
               falhas e a manutenção preventiva de sistemas elétricos.
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,7 +8,7 @@ function Home() {
   return (
     <>
       <div className="page-home page">
-        <Header/>
+        <Header />
         <section className="content">
           <img
             src="/src/assets/eletricistImg2.svg"
@@ -24,7 +24,7 @@ function Home() {
         <section className="content-init">
           <div className="content-eletrica">
             <div className="text-eletrica">
-              <h1 className="title">ELETHRONOS</h1>
+              <h2 className="title">ELETHRONOS</h2>
               <p className="description">
                 presta serviços a mais de 15 anos, sendo firmado como um dos
                 serviços mais sólidos do mercado,atuando no mercado com
@@ -40,9 +40,17 @@ function Home() {
             </div>
           </div>
         </section>
-        <CardEspecifications/>
+        <CardEspecifications />
         <section className="recent-projects">
-
+          <h3>Projetos recentes</h3>
+          <div className="projects">
+            <img src="/src/assets/projeto1.svg" alt="imagem projeto" />
+            <img src="/src/assets/projeto2.svg" alt="imagem projeto 2" />
+            <img src="/src/assets/projeto3.svg" alt="imagem projeto 3" />
+          </div>
+        </section>
+        <section className="feedbacks">
+          <h4>Feedbacks</h4>
         </section>
       </div>
     </>
